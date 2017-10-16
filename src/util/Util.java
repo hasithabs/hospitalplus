bef9d95847546648c87cdc6327d4f9d38fcfb74e
Kasun Madusanka <kmkasunmadushanka@GMAIL.COM>
2017-10-17 00:47:15 +0530

    Add property File Reading Class

--- src/util/Util.java
+++ src/util/Util.java
@@ -10,6 +10,8 @@ public class Util {
 
     private static final String ICONS_PATH = "/view/images/";
 
+    public static final String PROPERTY_FILE_PATH = System.getProperty("user.dir") + "\\src\\util\\App.properties";
+
     public static ImageIcon getIcon(Class<?> kclass, String icone) {
         return new ImageIcon(kclass.getResource(ICONS_PATH + icone + ".png"));
     }
