69998507a49a008711603c2fccae213cc2934779
Yashvida Jayasekara <yashvida.1007@gmail.com>
2017-10-22 23:44:09 +0530

    DBUtill error to Message Rnamed XML

--- src/util/Util.java
+++ src/util/Util.java
@@ -14,7 +14,7 @@ public class Util {
     
     public static final String QUERY_FILE_PATH = System.getProperty("user.dir") + "\\src\\util\\XMLFiles\\Query\\";
     
-    public static final String ERROR_FILE_PATH = System.getProperty("user.dir") + "\\src\\util\\XMLFiles\\Error\\";
+    public static final String MSG_FILE_PATH = System.getProperty("user.dir") + "\\src\\util\\XMLFiles\\Messages\\";
 
     public static ImageIcon getIcon(Class<?> kclass, String icone) {
         return new ImageIcon(kclass.getResource(ICONS_PATH + icone + ".png"));
